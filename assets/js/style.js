21de7bdcf8d7afada1ded648b3aeacea26fa6aa3
NamHyunJu <72422188+NamHyunJu@users.noreply.github.com>
2021-01-10 18:45:40 +0900

    js수정

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     var _window=$(window);
-    
+    var _skill=$('#skill');
     var _jump=$('#jump');
     var start,current,end;
     var step=20;
-    var jump=150;
+    var jump=460;
     var _guide=$('.guide');
     var _keyboard=_guide.find('.keyboard');
-    var one,two,three,chk=false;
+    var one,two,three,practice=false;
     var timer=0;
 
     //about me
@@ -31,7 +31,7 @@ $(document).ready(function(){
     });
 
     //스킬
-    $('#skill .txt').stop().slideUp();
+    _skill.find('.txt').stop().slideUp();
     
     //left에 가끔씩 오류가 생긴다 왜?
     start=_jump.position().left;
@@ -39,21 +39,20 @@ $(document).ready(function(){
     end=_window.width()-_jump.width()-30;
 
     _window.on('scroll',function(){
-        
         var scrollT=$(this).scrollTop();
         var skillP=$(document).height()-$('#me').height();
         clearTimeout(timer);
         timer=setTimeout(function(){
-            if(skillP+500<=scrollT && !chk){
-                _jump.attr({tabIndex:0}).focus();
-                chk=true;
+            if(skillP+500<=scrollT && !practice){
+                _jump.attr({tabIndex:0});
+                practice=true;
             }
         },50);
     });
     _window.on({
         //1번만 연습
         'keyup':function(e){
-            if(chk){
+            if(practice){
                 if(e.keyCode===39 && !one && !two && !three){
                     _keyboard.find('.right').removeClass('click').siblings('.left').addClass('click');
                     one=true;
@@ -68,32 +67,42 @@ $(document).ready(function(){
                 } else if(e.keyCode===32 && one && two && !three){
                     e.preventDefault();
                     _keyboard.find('.spacebar').removeClass('click');
-                    _jump.stop().animate({bottom:jump},300,function(){
+                    _jump.stop().animate({bottom:jump},500,function(){
                         $(this).stop().animate({bottom:30},800,'easeOutBounce');
                         _guide.stop().delay(1000).slideUp('fast');
                         three=true;
                     });
                 }
-    
             }
         },
         'keydown':function(e){
-            if(three){
-                if(e.keyCode===39 && current<=end){//오른쪽 방향키 눌렀을때
-                    current+=step;
-                    _jump.css({left:current});
-                }
-                if(e.keyCode===37 && current>=start){//왼쪽 방향키 눌렀을때
-                    current-=step;
-                    _jump.css({left:current});
-                }
-                if(e.keyCode===32){//스페이스바 눌렀을때
-                    e.preventDefault();
-                    _jump.stop().animate({bottom:jump},300,function(){
-                        $(this).stop().animate({bottom:30},800,'easeOutBounce');
-                    });
-                }    
+            var x=138.8;
+            var y=(_window.width()-1400)/2;
+            var z=x+y-15;
+            var hexWid=_skill.find('.hexagon').outerWidth();
+            
+            if(e.keyCode===39 && current<=end && three){
+                current+=step;
+                _jump.css({left:current});
+                
             }
+            if(e.keyCode===37 && current>=start && three){
+                current-=step;
+                _jump.css({left:current});
+            }
+            if(e.keyCode===32 && three){
+                e.preventDefault();
+                _jump.stop().animate({bottom:jump},500,function(){
+                    $(this).stop().animate({bottom:30},800,'easeOutBounce');
+                });
+                if(z<=current && current<=z+hexWid){
+                    _skill.find('.programming .txt').stop().delay(510).slideDown();
+                } else if((2*x)+z+hexWid<=current && current<=(2*x)+z+(2*hexWid)){
+                    _skill.find('.design .txt').stop().delay(510).slideDown();
+                }else if((4*x)+z+(2*hexWid)<=current && current<=(4*x)+z+(3*hexWid)){
+                    _skill.find('.ect .txt').stop().delay(510).slideDown();
+                }
+            }    
         }
     });
     
