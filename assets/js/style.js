d9e6436048ff1e382dafb929a70d1cb082aacbc5
NamHyunJu <72422188+NamHyunJu@users.noreply.github.com>
2021-01-12 04:07:35 +0900

    01-12 오전4시
    report추가

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,9 +2,57 @@ $(document).ready(function(){
     //project
     $('#what,#kids').attr({'aria-hidden':true}).stop().slideUp();
     $('.controller .next').on('click',function(){
-        $(this).parents('article').attr({'aria-hidden':true}).stop().slideUp().next().attr({'aria-hidden':false}).stop().slideDown();
+        if($(this).parents('article').is('#kids')){
+            $(this).parents('article').attr({'aria-hidden':true}).stop().slideUp().prev().prev().attr({'aria-hidden':false}).stop().slideDown();
+        } else{
+            $(this).parents('article').attr({'aria-hidden':true}).stop().slideUp().next().attr({'aria-hidden':false}).stop().slideDown();
+        }
     });
     $('.controller .prev').on('click',function(){
-        $(this).parents('article').attr({'aria-hidden':true}).stop().slideUp().prev().attr({'aria-hidden':false}).stop().slideDown();
+        if($(this).parents('article').is('#jaenung')){
+            $(this).parents('article').attr({'aria-hidden':true}).stop().slideUp().next().next().attr({'aria-hidden':false}).stop().slideDown();
+        } else{
+            $(this).parents('article').attr({'aria-hidden':true}).stop().slideUp().prev().attr({'aria-hidden':false}).stop().slideDown();
+        }
+    });
+
+    //report
+    $('.tab:first-of-type, .tabpanel:first-of-type').addClass('active').attr({tabIndex:0});
+    $('.tab:first-of-type').attr({'aria-selected':true}).siblings().attr({'aria-selected':false});
+    $('.tabpanel:first-of-type').attr({'aria-hidden':false}).siblings('.tabpanel').attr({'aria-hidden':true});
+
+    
+    $('.tab').on({
+        'click':function(){
+            $(this).addClass('active').attr({tabIndex:0,'aria-selected':true}).siblings('.active').removeClass('active').attr({tabIndex:-1,'aria-selected':false});
+
+            var tabNum=$('.tab').index();
+            $('.tabpanel').eq(tabNum).addClass('active').attr({tabIndex:0,'aria-hidden':false}).siblings('.active').removeClass('active').attr({tabIndex:-1,'aria-hidden':true});
+        },
+        //오른쪽(39) 왼쪽(37) home(36) end(35) 엔터(13) 스페이스바(32)
+        'keydown':function(e){
+            var key=e.keyCode;
+            switch(key){
+                case 39:
+                $(this).attr({tabIndex:-1});
+                if($(this).hasClass('last')) {$(this).siblings('.first').attr('tabIndex',0).focus();} else {$(this).next().attr('tabIndex',0).focus();}
+            break;
+            case 37:
+                $(this).attr({tabIndex:-1});
+                if($(this).hasClass('first')) {$(this).siblings('.last').attr({tabIndex:0}).focus();} else{$(this).prev().attr({tabIndex:0}).focus();}
+            break;
+            case 36:
+                e.preventDefault();
+                $(this).parent().find('.first').attr({tabIndex:0}).focus();
+            break;
+            case 35:
+                e.preventDefault();
+                $(this).parent().find('.last').attr({tabIndex:0}).focus();
+            break;
+            case 13:
+            case 32:
+                $(this).trigger('click');
+            }
+        }
     });
 });
